caa17889318f3d553cc1d068f7810b8a06770aa3
Jakub Kubera <jakub.kubera@zdt.io>
2024-03-21 10:57:54 +0100

    added create redis database example

---
+++ examples/create_redis_database/main.tf
@@ -0,0 +1,12 @@
+module "Digitalocean_mysql" {
+  source                = "github.com/opsd-io/terraform-module-digitalocean-mysql?ref=1-create-redis-terraform-module"
+  databasename          = "example-redis-cluster"
+  engine                = "redis"
+  version_of_engine     = "7"
+  mysql_main_size       = "db-s-1vcpu-1gb"
+  region                = "nyc1"
+  mysql_main_node_count = 1
+  common_tags           = ["production"]
+  database_users        = ["Admin", "Mark", "Robert"]
+  firewall_rules        = ["192.168.0.1", "192.168.0.2", "192.168.0.8"]
+}
