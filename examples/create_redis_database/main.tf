e28017e0ca3b0be0a369cc296ded4091e76f1856
Jakub Kubera <jakub.kubera@zdt.io>
2024-03-25 13:54:13 +0100

    fixed node_count variable in example

--- examples/create_redis_database/main.tf
+++ examples/create_redis_database/main.tf
@@ -1,10 +1,10 @@
 module "Digitalocean_redis_example" {
-  source                = "github.com/opsd-io/terraform-module-digitalocean-redis?ref=1-create-redis-terraform-module"
-  cluster_name          = "example-redis-cluster"
-  redis_version         = "7"
-  node_size             = "db-s-1vcpu-1gb"
-  region                = "nyc1"
-  redis_main_node_count = 1
-  common_tags           = ["production"]
-  firewall_rules        = ["192.168.0.1", "192.168.0.2", "192.168.0.8"]
+  source         = "github.com/opsd-io/terraform-module-digitalocean-redis?ref=1-create-redis-terraform-module"
+  cluster_name   = "example-redis-cluster"
+  redis_version  = "7"
+  node_size      = "db-s-1vcpu-1gb"
+  region         = "nyc1"
+  node_count     = 1
+  common_tags    = ["production"]
+  firewall_rules = ["192.168.0.1", "192.168.0.2", "192.168.0.8"]
 }
